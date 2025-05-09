2d4a902c6b5793b5aeec9d6e9be799a5889ea43f
Ricky.Pratama <pratamaricky@gmail.com>
2025-05-09 19:02:34 +0700

    adadoaisdoas

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -47,7 +47,7 @@
   <div id="customCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="{{ asset('bs/assets/images/sliderbanner.png') }}" class="d-block w-100" alt="Slide 1">
+        <img src="{{ asset('bs/assets/images/sliderbanner2.png') }}" class="d-block w-100" alt="Slide 1">
       </div>
       {{-- <div class="carousel-item">
         <img src="{{ asset('bs/assets/images/5.png') }}" class="d-block w-100" alt="Slide 2">
