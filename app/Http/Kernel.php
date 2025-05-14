21ccf452c8ea320c85335dc420319495d4f9c715
Ricky.Pratama <pratamaricky@gmail.com>
2025-05-14 22:43:10 +0700

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,7 @@ class Kernel extends HttpKernel
     ];
 
     protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
