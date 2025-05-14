e1a9939b37fc91da59a62d4258337e1f268d2ffa
Ricky.Pratama <pratamaricky@gmail.com>
2025-05-14 23:13:08 +0700

    role

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,11 +8,19 @@ use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
-    public function handle(Request $request, Closure $next, $role)
-    {
-        if (!Auth::check() || Auth::user()->role !== $role) {
-            return redirect('/home')->with('error', 'Anda tidak memiliki akses!');
-        }
-        return $next($request);
+    public function handle($request, Closure $next, $roles)
+{
+    $rolesArray = explode(',', $roles);
+
+    // Debugging
+    if (!Auth::check()) {
+        abort(403, 'User not authenticated');
     }
+
+    if (!in_array(Auth::user()->role, $rolesArray)) {
+        abort(403, 'Role not matched');
+    }
+
+    return $next($request);
+}
 }
