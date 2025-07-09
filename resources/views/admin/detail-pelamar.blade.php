7873b7e750b48cb854f54691b9863d5c4fa2f05d
Ricky.Pratama <pratamaricky@gmail.com>
2025-07-09 16:20:04 +0700

    update lagi

--- resources/views/admin/detail-pelamar.blade.php
+++ resources/views/admin/detail-pelamar.blade.php
@@ -14,71 +14,52 @@
                             <div class="card-body table-responsive p-0" style="overflow-x: auto;">
                                 <table id="example1" class="table table-bordered table-striped">
                                     <thead>
-                                        <tr class="header-row">
-                                            <th class="align-top text-center" rowspan="2">No</th>
-                                            <th class="align-top text-center" rowspan="2">Foto</th>
-                                            <th class="align-top text-center" rowspan="2" style="width: 250px;">Nama</th>
-                                            <th class="align-top text-center" rowspan="2" style="width: 250px;">NIK</th>
-                                            <th class="align-top text-center" rowspan="2">Calon Kalangan</th>
-                                            <th class="align-top text-center" rowspan="2">No. Handphone</th>
-                                            <th class="align-top text-center" rowspan="2">Email</th>
+  <tr>
+    <th class="text-center">No</th>
+    <th class="text-center">Foto</th>
+    <th class="text-center" style="width: 250px;">Nama</th>
+    <th class="text-center" style="width: 250px;">NIK</th>
+    <th class="text-center">Calon Kalangan</th>
+    <th class="text-center">No. Handphone</th>
+    <th class="text-center">Email</th>
 
-                                            <!-- Header Pengusul Calon Kalangan -->
-                                            {{-- <th class="align-top text-center" colspan="4">Pengusul Calon Kalangan</th> --}}
-                                            <!-- Header Pendidikan -->
-                                            <th class="align-top text-center" colspan="3">Pendidikan</th>
+    {{-- Pendidikan --}}
+    <th class="text-center" style="width: 200px;">Sarjana</th>
+    <th class="text-center" style="width: 200px;">Magister</th>
+    <th class="text-center" style="width: 200px;">Doktoral</th>
 
-                                            <!-- Header Pengalaman Kerja -->
-                                            <th class="align-top text-center" colspan="3">Pengalaman Kerja Terakhir</th>
-                                        </tr>
+    {{-- Pengalaman kerja --}}
+    <th class="text-center">Pengalaman</th>
+  </tr>
+</thead>
 
-                                        <tr class="header-row">
-                                            <!-- Pendidikan -->
-                                        <th class="align-top text-center" style="width: 200px;">Sarjana</th>
-                                        <th class="align-top text-center" style="width: 200px;">Magister</th>
-                                        <th class="align-top text-center" style="width: 200px;">Doktoral</th>
-                                            <!-- Sub-header Pengusul Calon Kalangan -->
-                                            {{-- <th class="align-top text-center">Organisasi Pengusul</th>
-                                            <th class="align-top text-center">Rekomendasi Pakar-1</th>
-                                            <th class="align-top text-center">Rekomendasi Pakar-2</th>
-                                            <th class="align-top text-center">Rekomendasi Pakar-3</th> --}}
-
-                                            <!-- Sub-header Pengalaman Kerja -->
-                                            <th class="align-top text-center">Jabatan</th>
-                                            <th class="align-top text-center">Unit Kerja</th>
-                                            <th class="align-top text-center">TMT Jabatan</th>
-                                        </tr>
-                                    </thead>
                                     <tbody>
     @forelse ($data as $nourut => $pel)
         @php
             $experiences = $pel->userExperiences->sortByDesc('tmt_jabatan');
-            $rowspan = max($experiences->count(), 1);
-
             $gelardepan = $pel->userProfile?->gelar_depan !== '-' ? $pel->userProfile?->gelar_depan : '';
             $gelarbelakang = $pel->userProfile?->gelar_belakang !== '-' ? $pel->userProfile?->gelar_belakang : '';
             $namalengkap = trim("{$gelardepan} {$pel->name}" . ($gelarbelakang ? ", {$gelarbelakang}" : ''));
             $berkaspelamar = \App\Helpers\Bantuan::berkaspelamar($pel->id)->first();
         @endphp
 
-        {{-- Baris pertama --}}
         <tr>
-            <td rowspan="{{ $rowspan }}">{{ $nourut + 1 }}</td>
-            <td rowspan="{{ $rowspan }}">
+            <td>{{ $nourut + 1 }}</td>
+            <td>
                 @if ($pel->userProfile?->pas_foto)
                     <img src="{{ asset('uploads/pas_foto/' . $pel->userProfile->pas_foto) }}" width="100">
                 @else
                     <span class="badge bg-danger">Belum diisi</span>
                 @endif
             </td>
-            <td rowspan="{{ $rowspan }}">{{ $namalengkap }}</td>
-            <td rowspan="{{ $rowspan }}">{{ $pel->nik ?? '' }}</td>
-            <td rowspan="{{ $rowspan }}">{{ $pel->userProfile?->kalangan ?? '' }}</td>
-            <td rowspan="{{ $rowspan }}">{{ $pel->userProfile?->no_handphone ?? '' }}</td>
-            <td rowspan="{{ $rowspan }}">{{ $pel->email ?? '' }}</td>
+            <td>{{ $namalengkap }}</td>
+            <td>{{ $pel->nik ?? '-' }}</td>
+            <td>{{ $pel->userProfile?->kalangan ?? '-' }}</td>
+            <td>{{ $pel->userProfile?->no_handphone ?? '-' }}</td>
+            <td>{{ $pel->email ?? '-' }}</td>
 
             {{-- Pendidikan --}}
-            <td rowspan="{{ $rowspan }}" class="text-center">
+            <td class="text-center">
                 @if ($berkaspelamar)
                     <strong>{{ $berkaspelamar->universitas_sarjana }}</strong><br>
                     {{ $berkaspelamar->jurusan_sarjana }}<br>
@@ -87,7 +68,7 @@
                     <span class="badge bg-danger">Belum diisi</span>
                 @endif
             </td>
-            <td rowspan="{{ $rowspan }}" class="text-center">
+            <td class="text-center">
                 @if ($berkaspelamar)
                     <strong>{{ $berkaspelamar->universitas_magister }}</strong><br>
                     {{ $berkaspelamar->jurusan_magister }}<br>
@@ -96,7 +77,7 @@
                     <span class="badge bg-danger">Belum diisi</span>
                 @endif
             </td>
-            <td rowspan="{{ $rowspan }}" class="text-center">
+            <td class="text-center">
                 @if ($berkaspelamar)
                     <strong>{{ $berkaspelamar->universitas_doktoral }}</strong><br>
                     {{ $berkaspelamar->jurusan_doktoral }}<br>
@@ -106,31 +87,30 @@
                 @endif
             </td>
 
-            {{-- Pengalaman pertama --}}
-            @if ($experiences->count())
-                @php $firstExp = $experiences->first(); @endphp
-                <td>{{ $firstExp->nama_jabatan ?? 'Belum diisi' }}</td>
-                <td>{{ $firstExp->unit_kerja ?? 'Belum diisi' }}</td>
-                <td>{{ $firstExp->tmt_jabatan ? \Carbon\Carbon::parse($firstExp->tmt_jabatan)->format('d/m/Y') : 'Belum diisi' }}</td>
-            @else
-                <td colspan="3" class="text-center">Tidak ada pengalaman kerja</td>
-            @endif
+            {{-- Pengalaman kerja dalam satu kolom --}}
+            <td colspan="3">
+                @if ($experiences->isNotEmpty())
+                    <ol class="mb-0 ps-3">
+                        @foreach ($experiences as $i => $exp)
+                            <li>
+                                <strong>{{ $exp->nama_jabatan ?? 'Belum diisi' }}</strong><br>
+                                {{ $exp->unit_kerja ?? 'Belum diisi' }}<br>
+                                {{ $exp->tmt_jabatan ? \Carbon\Carbon::parse($exp->tmt_jabatan)->format('d/m/Y') : 'Belum diisi' }}
+                            </li>
+                        @endforeach
+                    </ol>
+                @else
+                    <span class="badge bg-warning text-dark">Tidak ada pengalaman</span>
+                @endif
+            </td>
         </tr>
-
-        {{-- Baris tambahan untuk pengalaman ke-2 dst --}}
-        @foreach ($experiences->skip(1) as $exp)
-            <tr>
-                <td>{{ $exp->nama_jabatan ?? 'Belum diisi' }}</td>
-                <td>{{ $exp->unit_kerja ?? 'Belum diisi' }}</td>
-                <td>{{ $exp->tmt_jabatan ? \Carbon\Carbon::parse($exp->tmt_jabatan)->format('d/m/Y') : 'Belum diisi' }}</td>
-            </tr>
-        @endforeach
     @empty
         <tr>
-            <td colspan="15" class="text-center">Data tidak tersedia</td>
+            <td colspan="14" class="text-center">Data tidak tersedia</td>
         </tr>
     @endforelse
 </tbody>
+
 
                                 </table>
                             </div>
