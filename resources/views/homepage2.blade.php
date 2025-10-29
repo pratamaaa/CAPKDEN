40b2b4571962c9061ced42150699192f4e5c374e
Ricky.Pratama <pratamaricky@gmail.com>
2025-10-29 17:04:24 +0700

    upd

--- resources/views/homepage2.blade.php
+++ resources/views/homepage2.blade.php
@@ -1,6 +1,6 @@
 @extends('layout/main')
 @section('content')
-    <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+    {{-- <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
         <div class="container">
             <div class="">
                 <div class="" data-wow-duration="1s" data-wow-delay="0.5s">
@@ -8,7 +8,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     {{-- <div id="free-quote" class="free-quote">
         <div class="container">
